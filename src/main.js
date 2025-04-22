5d35d9d096c2088cd0978aa03ee008358b9ed389
sum-bocfob <43840414+sum-bocfob@users.noreply.github.com>
2025-04-22 17:16:48 +0900

    🐛 ヘッダーバグ修正

--- src/main.js
+++ src/main.js
@@ -7,12 +7,26 @@ $(function () {
     const ham = $(".l-header__hamburger");
     const gnav = $(".l-header__gnav");
     const menu_bg = $(".l-header__menu-bg");
+    const anim_dur = 0.3;
     ham.on("click", function () {
         $(this).toggleClass("js-open-menu");
         gnav.toggleClass("js-open-menu");
         menu_bg.toggleClass("js-open-menu");
         ham.attr("aria-expanded", ham.attr("aria-expanded") === "true" ? "false" : "true");
         gnav.attr("aria-hidden", gnav.attr("aria-hidden") === "true" ? "false" : "true");
+    });
+    menu_bg.on("click", function () {
+        console.log("tes");
+        ham.removeClass("js-open-menu");
+        gnav.removeClass("js-open-menu");
+        $(this).removeClass("js-open-menu");
+    });
+
+    // メニューからページ遷移
+    $(".l-header__link").on("click", function () {
+        ham.css("transitionDuration", "0s").removeClass("js-open-menu").css(`transitionDuration, ${anim_dur}`);
+        gnav.css("transitionDuration", "0").removeClass("js-open-menu").css(`transitionDuration, ${anim_dur}`);
+        menu_bg.css("transitionDuration", "0").removeClass("js-open-menu").css(`transitionDuration, ${anim_dur}`);
     });
 
     //* inView
